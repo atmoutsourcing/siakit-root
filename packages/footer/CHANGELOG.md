9bc4953c011b7b5e6858433433f8fe68da7d32d6
Bruno Fabre <bruno.hfabre@gmail.com>
2022-10-21 12:06:12 -0300

    add display flex to footer left

--- packages/footer/CHANGELOG.md
+++ packages/footer/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @siakit/footer
 
+## 0.0.2
+
+### Patch Changes
+
+- add display flex to footer left
+
 ## 0.0.1
 
 ### Patch Changes
--- packages/footer/src/FooterLeft.ts
+++ packages/footer/src/FooterLeft.ts
@@ -1,6 +1,8 @@
 import { styled } from '@siakit/core'
 
 export const FooterLeft = styled('div', {
+  display: 'flex',
+
   flex: 1,
   gap: 8,
 })
