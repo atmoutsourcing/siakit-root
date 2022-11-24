3be73902ebafb24a15284623ded43c9452f9a967
Bruno Fabre <bruno.hfabre@gmail.com>
2022-11-24 12:15:12 -0300

    fix shimmer size

--- packages/shimmer/src/Ellipse.tsx
+++ packages/shimmer/src/Ellipse.tsx
@@ -10,10 +10,10 @@ export function Ellipse({ width, height, ...props }: EllipseProps) {
     <EllipseContainer
       css={{
         width: typeof width === 'number' ? `${width}px` : width,
-        maxWidth: typeof width === 'number' ? `${width}px` : width,
+        minWidth: typeof width === 'number' ? `${width}px` : width,
 
         height: typeof height === 'number' ? `${height}px` : height,
-        maxHeight: typeof height === 'number' ? `${height}px` : height,
+        minHeight: typeof height === 'number' ? `${height}px` : height,
       }}
       {...props}
     />
--- packages/shimmer/src/Rectangle.tsx
+++ packages/shimmer/src/Rectangle.tsx
@@ -10,10 +10,10 @@ export function Rectangle({ width, height, ...props }: RectangleProps) {
     <RectangleContainer
       css={{
         width: typeof width === 'number' ? `${width}px` : width,
-        maxWidth: typeof width === 'number' ? `${width}px` : width,
+        minWidth: typeof width === 'number' ? `${width}px` : width,
 
         height: typeof height === 'number' ? `${height}px` : height,
-        maxHeight: typeof height === 'number' ? `${height}px` : height,
+        minHeight: typeof height === 'number' ? `${height}px` : height,
       }}
       {...props}
     />
