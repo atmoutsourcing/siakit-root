b877dd0a8d9604dc409df33fa66245318707789d
Bruno Fabre <bruno.hfabre@gmail.com>
2022-12-22 10:21:29 -0300

    fix

--- packages/docs/src/pages/TestPage.tsx
+++ packages/docs/src/pages/TestPage.tsx
@@ -1,41 +1,17 @@
-import * as yup from 'yup'
+import { useState } from 'react'
 
 import { Button } from '@siakit/button'
-import { Form, DatePicker, useForm } from '@siakit/form-unform'
+import { DatePicker } from '@siakit/form-components'
 import { Flex } from '@siakit/layout'
 
-const testFormSchema = yup.object({
-  teste: yup.string().required().nullable(),
-})
-
-type TestFormInputs = {
-  teste: string
-}
-
 export function TestPage() {
-  const { formRef, handleSubmit } = useForm<TestFormInputs>({
-    formSchema: testFormSchema,
-  })
-
-  function handleSubmitData(data: any) {
-    console.log(data)
-  }
+  const [date, setDate] = useState<Date | null>(null)
 
   return (
-    <Form ref={formRef} onSubmit={() => handleSubmit(handleSubmitData)}>
-      <DatePicker name="teste" label="teste" placeholder="teste" />
+    <Flex direction="column" gap padding>
+      <DatePicker value={date} onChange={setDate} />
 
-      <Flex>
-        <Button
-          type="button"
-          onClick={() =>
-            formRef.current?.setFieldValue('teste', new Date(1997, 5, 21))
-          }
-        >
-          set value
-        </Button>
-        <Button type="submit">submit</Button>
-      </Flex>
-    </Form>
+      <Button onClick={() => setDate(new Date())}>change value</Button>
+    </Flex>
   )
 }
--- packages/form-components/CHANGELOG.md
+++ packages/form-components/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @siakit/form-components
 
+## 0.0.16
+
+### Patch Changes
+
+- fix
+
 ## 0.0.15
 
 ### Patch Changes
